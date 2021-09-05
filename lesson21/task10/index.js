fe09a088c4cd0326d6a0e176dc2f6b8fde0ae21f
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-05 21:19:16 +0300

    task10

--- lesson21/task10/index.js
+++ lesson21/task10/index.js
@@ -1,7 +1,7 @@
 export const finishForm = ()=>{
-    const form = document.querySelector('.login-form');
-    document.querySelector('input').setAttribute('type',"password");
-    const login = document.createElement('input');
+    const form = document.querySelector(".login-form");
+    document.querySelector("input").setAttribute('type',"password");
+    const login = document.createElement("input");
     login.setAttribute('name',"login");
     login.setAttribute('type',"text");
     form.prepend(login)
