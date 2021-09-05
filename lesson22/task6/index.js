9107881415f03dffea8730f2f32dfce0e67fabfd
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-06 00:52:51 +0300

    task6

--- lesson22/task6/index.js
+++ lesson22/task6/index.js
@@ -1,3 +1,3 @@
 const buttonElem = document.querySelector('.single-use-btn');
     
-buttonElem.addEventListener('click',()=>{console.log('checked');},{once:true})
+buttonElem.addEventListener('click',()=>{console.log('clicked');},{once:true})
