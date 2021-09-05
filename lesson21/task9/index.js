6ea1c30f435ac8a9b5cede3377c93a8d07426ec2
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-05 18:33:07 +0300

    task9

--- lesson21/task9/index.js
+++ lesson21/task9/index.js
@@ -1,15 +1,19 @@
 export const finishList = ()=>{
-    const listItemOne = document.createElement('li');
-    listItemOne.textContent = '1'
     const special = document.querySelector('.special');
     const list = document.querySelector('.list');
+
+    const listItemOne = document.createElement('li');
+    listItemOne.textContent = '1'
     list.prepend(listItemOne);
+
     const listItemFour = document.createElement('li');
     listItemFour.textContent = '4';
     special.before(listItemFour);
+
     const listItemSix = document.createElement('li');
     listItemSix.textContent = '6';
     special.after(listItemSix);
+    
     const listItemEight = document.createElement('li');
     listItemEight.textContent = '8';
     list.append(listItemEight)
