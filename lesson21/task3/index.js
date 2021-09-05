86c822168af9500345746e2b99a576cb8087413d
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-05 17:16:38 +0300

    task3

---
+++ lesson21/task3/index.js
@@ -0,0 +1,11 @@
+export const getItemsList = ()=>{
+   const technoItem= document.querySelectorAll('.technology');
+   console.dir(technoItem);
+}
+export const getItemsArray = ()=>{
+   const technoItem= document.querySelectorAll('.tool');
+   const elementsArray = [...technoItem]
+   console.dir(elementsArray);
+   console.log(elementsArray);
+   return elementsArray;
+}
