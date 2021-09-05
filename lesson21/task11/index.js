c39077b1e83584836f1eee6eb0db4425ce664475
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-05 21:40:00 +0300

    task11

---
+++ lesson21/task11/index.js
@@ -0,0 +1,13 @@
+export const manageClasses = ()=>{
+  const one= document.querySelector('.one');
+  one.classList.add('selected')
+  const two = document.querySelector('.two');
+  two.classList.remove('selected')
+  const three = document.querySelector('.three');
+  three.classList.toggle('three_done')
+  const four = document.querySelector('.four');
+  if(four.classList.value.includes("some-class")){
+    four.classList.add('another-class')
+  }
+}
+manageClasses()
