26a5e4d2bd74e04cc0118af1e00ed153d5d10dfc
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-06 00:25:55 +0300

    task5

---
+++ lesson22/task5/index.js
@@ -0,0 +1,5 @@
+const buttonElem = document.querySelector('.search__btn');
+const inputElem = document.querySelector('.search__input');
+buttonElem.addEventListener('click',()=>{
+    console.log(inputElem.value)
+})
