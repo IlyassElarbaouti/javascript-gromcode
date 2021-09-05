b7b656287c1c88ee903b05e858dc9753b248cdcc
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-06 01:05:51 +0300

    task7

---
+++ lesson22/task7/index.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll('.btn');
+function handleClick(event){
+    console.log(event.target.textContent)
+}
+buttons.forEach(button=>{button.addEventListener('click',handleClick)})
