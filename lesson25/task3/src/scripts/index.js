b48ee39bdc49f74f4567f214fad671376e53cad1
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-09 22:47:01 +0300

    task3

---
+++ lesson25/task3/src/scripts/index.js
@@ -0,0 +1,17 @@
+import { initTodoListHandlers } from "./todoList.js";
+// eslint-disable-next-line import/no-unresolved
+import { renderTasks } from "./renderer.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderTasks();
+  initTodoListHandlers();
+});
+
+const onStorageChange = (e) => {
+  if (e.key !== "tasksList") {
+    return null;
+  }
+  renderTasks();
+};
+
+window.addEventListener("storage", onStorageChange);
