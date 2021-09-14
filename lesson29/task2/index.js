e70421aa92f0c43f1094cabf2b250d5237ad8596
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-14 07:28:10 +0300

    test

---
+++ lesson29/task2/index.js
@@ -0,0 +1,16 @@
+const pinger = (num,period)=>{
+    let i = num;
+ const interval =setInterval(()=>{
+     // eslint-disable-next-line no-plusplus
+     if(--i > 0){
+         console.log('Ping')
+     }
+     else{
+          clearInterval(interval)
+     }
+     
+    },period)
+
+}
+pinger(5,1000)
+
