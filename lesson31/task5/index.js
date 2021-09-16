f939cafed73d56f53468eddbedfd0dd586ceb712
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-16 12:27:35 +0300

    task1

--- lesson31/task5/index.js
+++
@@ -1,6 +0,0 @@
-export const delay = (time)=>
-    new Promise((resolve)=>{
-    setTimeout(()=>resolve(),time)
-})
-
-delay(3000).then(()=>{console.log('Done')})
---
+++ lesson32/task1/index.js
@@ -0,0 +1,22 @@
+ const getValueWithDelay = (value,delay)=>
+    new Promise((resolve)=>{
+    setTimeout(()=>{
+        console.log(value)
+        resolve(value)
+    },delay)})
+
+const asyncFunc1 = getValueWithDelay(1,1000);
+const asyncFunc2 = getValueWithDelay(2,2000);
+const asyncFunc3 = getValueWithDelay('hi',3000);
+const asyncFunc4 = getValueWithDelay(4,4000);
+const asyncFunc5 = getValueWithDelay(5,5000);
+
+const getSum = (arr)=>
+   arr.filter(number=>!isNaN(number)).reduce((acc,num)=>acc+Number(num),0);
+
+
+const asyncSum = (...asyncFuncs)=>
+  Promise.all(asyncFuncs).then(numbers=>getSum(numbers));
+
+  // testing
+asyncSum(asyncFunc1,asyncFunc2,asyncFunc3,asyncFunc4,asyncFunc5).then(result=>console.log(result));
