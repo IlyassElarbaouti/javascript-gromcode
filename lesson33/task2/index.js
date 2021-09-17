5e8d99a567907086627105c8218f933b593d4131
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-17 13:15:04 +0300

    task2

--- lesson33/task2/index.js
+++ lesson33/task2/index.js
@@ -1,23 +1,25 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-export function getTasksList() {
+
+function getTasksList() {
   // getTasksList logic
   return fetch(baseUrl)
   .then(data=>data.json())
 }
 
-export function getTaskById(taskId) {
+function getTaskById(taskId) {
   //  getTaskById logic
-  return getTasksList()
-  .then(usersArr=>usersArr.find(user=>user.id===taskId))
+  const taskUrl = `https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks/${taskId}`;
+  return fetch(taskUrl)
+  .then(data=>data.json())
   
 }
 
-// // examples
-// getTasksList().then(tasksList => {
-//   console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
-// });
+// examples
+getTasksList().then(tasksList => {
+  console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
+});
 
-// getTaskById('3').then(taskData => {
-//   console.log(taskData); // {'id':'2', 'done':true ... }
-// });
+getTaskById('3').then(taskData => {
+  console.log(taskData); // {'id':'2', 'done':true ... }
+});
