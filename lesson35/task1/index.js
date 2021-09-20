dc49435a71acdd6e556c788bf5409466fecc2427
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-20 14:21:25 +0300

    task1

---
+++ lesson35/task1/index.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line prefer-arrow-callback
+window.addEventListener("error", function onUnhandledError (err) {
+    console.error("error", err.message);
+});
+
+let userParsingResult;
+
+try {
+    const user = JSON.parse('{"name":"Ilyass"}');
+    console.log("User data: ", user);
+    userParsingResult = "success";
+} catch(e) {
+    userParsingResult = "error";
+} finally {
+    console.log(`User parsing finished with ${userParsingResult}`);
+}
+
+let productParsingResult;
+
+try {
+    const product = JSON.parse('{"name":"Ilyass"');
+    console.log("Product data: ", product);
+    productParsingResult = "success";
+} catch(e) {
+    productParsingResult = "error";
+} finally {
+    console.log(`Product parsing finished with ${productParsingResult}`);
+}
