05c4ddc6e4337da62ef27824d95e7fa2d586c5a2
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-26 21:47:34 +0300

    task1

---
+++ webpack/lesson6/task1/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry : './src/index.js',
+    output:{
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[{
+            test:/.css$/,
+            use:['style-loader','css-loader']
+        },
+        {
+            test:/.scss$/,
+            use:['style-loader','css-loader','scss-loader']
+        },
+        {
+            test:/.(jpg|png)$/,
+            use:[{
+                loader:'url-loader',
+                options:{
+                    limit:8192
+                }
+            }]
+        }]
+    }
+}
