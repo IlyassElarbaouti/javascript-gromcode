a3f97aa305eb381362e4432b930a44871d56a763
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-26 23:12:43 +0300

    task1

---
+++ webpack/lesson7/task1/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin= require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { default: miniExtractPluginFactory } = require('mini-extract-plugin');
+module.exports = {
+    entry : './src/index.js',
+    output:{
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[{
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              outputPath: 'images',
+            },
+          },
+        ],
+       
+      }]
+    },
+     plugins:[
+            new HtmlWebpackPlugin({
+                template:'./src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename:'[name].css'
+            })
+        ]
+}
