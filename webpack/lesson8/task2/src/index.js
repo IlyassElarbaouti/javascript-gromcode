023c037f7eb0c88997df4512c00fbf9e0b899eca
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-27 00:05:55 +0300

    task2

---
+++ webpack/lesson8/task2/src/index.js
@@ -0,0 +1,16 @@
+import { renderTasks } from './list/render.js';
+import { initHandlers } from './list/initHandlers.js';
+import './index.scss'
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderTasks();
+  initHandlers();
+});
+
+const onStorageChange = event => {
+  if (event.key === 'tasksList') {
+    renderTasks();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
---
+++ webpack/lesson8/task2/webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin= require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports =(env,argv)=> {
+  const isProduction = argv.mode === 'production'
+  const config = {
+    entry : './src/index.js',
+    output:{
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+          {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+          {
+        test: /\.s?css$/i,
+        use: [isProduction
+          ?MiniCssExtractPlugin.loader
+          :'style-loader'
+          , 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { 
+              limit: 8192,
+              outputPath: 'images',
+            },
+          },
+        ],
+      },]
+    },
+    plugins:[
+            new HtmlWebpackPlugin({
+                template:'./src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename:'[name].css'
+            }),
+            new CleanWebpackPlugin()
+          
+        ]
+      }
+      if(isProduction){
+        config.plugins.push(
+         new MiniCssExtractPlugin({
+           filename:'[name].css'
+         })
+        )
+      }
+      return config;
+}
